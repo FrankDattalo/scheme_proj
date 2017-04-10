8f628e3ca0510c52c5cb9a1293e0785fcf85ef21
Frank Dattalo <dattalo.2@osu.edu>
2017-04-10 19:08:51 -0400

    refactoring

--- eliminateN.scm
+++ eliminateN.scm
@@ -1,11 +1,11 @@
 ; Author: Frank Dattalo
 
 ; The following redefines serve no purpose other than
-; for the way that I think about the operations in my head.
+; for the way that I think about the operations in my first.
 ; I prefer the semantic names that I have the operations,
 ; and thus is why I renamed them.
-(define (head lst)   (car lst))
-(define (tail lst)   (cdr lst))
+(define (first lst)  (car lst))
+(define (rest lst)   (cdr lst))
 (define (pair p1 p2) (cons p1 p2))
 
 ; filter-list filters a list based off of a passed predicate.
@@ -19,8 +19,8 @@
 (define (filter-list l2f pf)
   (cond 
     ((null? l2f)      l2f)
-    ((pf (head l2f))  (pair (head l2f) (filter-list (tail l2f) pf)))
-    (#t               (                 filter-list (tail l2f) pf))))
+    ((pf (first l2f)) (pair (first l2f) (filter-list (rest l2f) pf)))
+    (#t               (                 filter-list (rest l2f) pf))))
 
 ; list-concat concatenates two lists together such as 
 ; (l1[1] l1[2] . . l1[N-1] l1[N] l2[1] l2[2] . . . l2[N-1] l2[N])
@@ -35,7 +35,7 @@
 (define (list-concat l1 l2)
   (cond 
     ((null? l1) l2)
-    (#t         (pair (head l1) (list-concat (tail l1) l2)))))
+    (#t         (pair (first l1) (list-concat (rest l1) l2)))))
 
 ; quicksort sorts a list by recorsively partitioning the list based off of a pivot value
 ; 
@@ -48,8 +48,8 @@
     (#t         
       (list-concat
       (list-concat 
-        (quick-sort (filter-list (tail l1) (lambda (i) (<  i (head l1))))) (pair (head l1) '()))
-        (quick-sort (filter-list (tail l1) (lambda (i) (>= i (head l1)))))))))
+        (quick-sort (filter-list (rest l1) (lambda (i) (<  i (first l1))))) (pair (first l1) '()))
+        (quick-sort (filter-list (rest l1) (lambda (i) (>= i (first l1)))))))))
 
 
 ; eliminate takes two lists and produces a list which contains all elements
@@ -64,9 +64,9 @@
   (cond
     ((null? l1) l1)
     ((null? l2) l1)
-    ((< (head l1) (head l2)) (pair (head l1) (eliminate (tail l1) l2)))
-    ((= (head l1) (head l2)) (                eliminate (tail l1) l2))
-    ((> (head l1) (head l2)) (                eliminate l1 (tail l2)))))
+    ((< (first l1) (first l2)) (pair (first l1) (eliminate (rest l1) l2)))
+    ((= (first l1) (first l2)) (                 eliminate (rest l1) l2))
+    ((> (first l1) (first l2)) (                 eliminate l1        (rest l2)))))
 
 
 ; eliminate-and-sort takes two lists and produces a sorted list which contains all elements
